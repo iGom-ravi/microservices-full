5836024ec15a9c0c5db3666d103956e49efd3a4c
iGom-ravi <95466376+iGom-ravi@users.noreply.github.com>
2022-12-22 15:59:44 +0530

    Delete microserices-demo directory

--- microserices-demo/demo1/src/main/java/com/example/demo1/demo1/repository/AccountRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo1.demo1.repository;
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.demo1.demo1.values.*;
-
-public interface AccountRepository extends CrudRepository<AccountOpenBalance,Integer>{
-
-}
--- microserices-demo/demo2/src/main/java/com/example/demo2/demo2/repository/FundTransferRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo2.demo2.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo2.demo2.values.FundTransferValues;
-
-public interface FundTransferRepository extends JpaRepository<FundTransferValues,Integer>{
-
-}
